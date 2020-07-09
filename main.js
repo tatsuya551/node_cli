1760eded54475e8e84b9747fd76bf5264bc979ec
Seki Tatsuya <seki0515ya@gmail.com>
2020-07-09 16:15:03 +0900

    node cli

---
+++ main.js
@@ -0,0 +1,25 @@
+// const myModule = require("./my-module");
+// console.log(myModule);
+
+const program = require("commander");
+const fs = require("fs");
+const md2html = require("./md2html");
+
+program.option("--gfm", "GFMを有効にする");
+program.parse(process.argv);
+const filePath = program.args[0];
+
+const cliOptions = {
+  gfm: false,
+  ...program.opts(),
+};
+
+fs.readFile(filePath, { encoding: "utf8" }, (err, file) => {
+  if (err) {
+    console.log(err.message);
+    process.exit(1);
+    return;
+  }
+  const html = md2html(file, cliOptions);
+  console.log(html);
+});
